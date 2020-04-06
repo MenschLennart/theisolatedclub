d5c576186b85383268cfec00b5da3d70e7430090
MenschLennart <55825932+MenschLennart@users.noreply.github.com>
2020-04-06 19:55:35 +0200

    Fix wrong migration file

--- database/migrations/2020_04_06_142503_drop_column_name_user.php
+++ database/migrations/2020_04_06_142503_drop_column_name_user.php
@@ -15,7 +15,7 @@ class DropColumnNameUser extends Migration
     {
         //
         Schema::table('users', function (Blueprint $table) {
-            $table->removeColumn('name');
+            $table->dropColumn('name');
         });
     }
 
