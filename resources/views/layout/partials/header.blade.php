259db07370784b80e2a76008a40ccec8fb15419d
MenschLennart <55825932+MenschLennart@users.noreply.github.com>
2020-04-07 23:48:57 +0200

    Fixed strange behaviour of nav toggler icon

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -1,11 +1,9 @@
-<section class="colorful">
-    <div class="jumbotron text-center">
-        <div class="container">
-            <h1 class="jumbotron-heading colorful">The Isolated Club</h1>
-            <p class="lead text-muted">Most of us #stayhome and ask ourselves: What do I do now as I am isolated at home?
-                Here you can find all kinds of links to apps, websites, exercises and games that support you during a
-                worldwide state of emergency.</p>
-            <p class="lead text-muted">And if you know a great thing you want to share with us, click the button!</p>
-        </div>
+<div class="jumbotron text-center">
+    <div class="container">
+        <h1 class="jumbotron-heading colorful">The Isolated Club</h1>
+        <p class="lead text-muted">Most of us #stayhome and ask ourselves: What do I do now as I am isolated at home?
+            Here you can find all kinds of links to apps, websites, exercises and games that support you during a
+            worldwide state of emergency.</p>
+        <p class="lead text-muted">And if you know a great thing you want to share with us, click the button!</p>
     </div>
-</section>
+</div>
--- resources/views/layout/partials/nav.blade.php
+++ resources/views/layout/partials/nav.blade.php
@@ -1,63 +1,61 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            TIC
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
-                aria-controls="navbarToggler" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="{{ route('home') }}">
+        TIC
+    </a>
+    <button class="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="custom-toggler navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarToggler">
-            <!-- Left side of navbar -->
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
-                    <a class="nav-link" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="{{ route('categories.index') }}">Categories <span class="sr-only">(current)</span></a>
-                </li>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left side of navbar -->
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('categories.index') }}">Categories <span
+                        class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-muted" target="_blank"
+                   href="https://twitter.com/hashtag/StayHome">#stayhome</a>
+            </li>
+        </ul>
+
+        <!-- Right side of navbar -->
+        <ul class="navbar-nav ml-auto">
+
+            <!-- Authentication Links -->
+            @guest
                 <li class="nav-item">
-                    <a class="nav-link text-muted" target="_blank"
-                       href="https://twitter.com/hashtag/StayHome">#stayhome</a>
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                 </li>
-            </ul>
-
-            <!-- Right side of navbar -->
-            <ul class="navbar-nav ml-auto">
-
-                <!-- Authentication Links -->
-                @guest
+                @if (Route::has('register'))
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                     </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->username }} <span class="caret"></span>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->username }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('myActivities') }}">{{ __('My Activities') }}</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
                         </a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('myActivities') }}">{{ __('My Activities') }}</a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+        </ul>
     </div>
 </nav>
