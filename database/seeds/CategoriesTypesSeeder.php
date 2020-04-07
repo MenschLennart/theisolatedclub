e14f9b331a8dbed5882bc34d3b3fa3541d8a2194
MenschLennart <55825932+MenschLennart@users.noreply.github.com>
2020-04-07 15:36:29 +0200

    Added images and database data for new categories

---
+++ database/seeds/CategoriesTypesSeeder.php
@@ -0,0 +1,108 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriesTypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories_types')->insert([
+            [
+                'category_id' => 1,
+                'type_id' => 1
+            ], [
+                'category_id' => 1,
+                'type_id' => 2
+            ], [
+                'category_id' => 1,
+                'type_id' => 3
+            ], [
+                'category_id' => 1,
+                'type_id' => 4
+            ], [
+                'category_id' => 1,
+                'type_id' => 5
+            ], [
+                'category_id' => 2,
+                'type_id' => 1
+            ], [
+                'category_id' => 2,
+                'type_id' => 2
+            ], [
+                'category_id' => 2,
+                'type_id' => 3
+            ], [
+                'category_id' => 2,
+                'type_id' => 4
+            ], [
+                'category_id' => 2,
+                'type_id' => 5
+            ], [
+                'category_id' => 3,
+                'type_id' => 1
+            ], [
+                'category_id' => 3,
+                'type_id' => 2
+            ], [
+                'category_id' => 3,
+                'type_id' => 3
+            ], [
+                'category_id' => 3,
+                'type_id' => 4
+            ], [
+                'category_id' => 3,
+                'type_id' => 5
+            ], [
+                'category_id' => 4,
+                'type_id' => 1
+            ], [
+                'category_id' => 4,
+                'type_id' => 2
+            ], [
+                'category_id' => 4,
+                'type_id' => 3
+            ], [
+                'category_id' => 4,
+                'type_id' => 4
+            ], [
+                'category_id' => 4,
+                'type_id' => 5
+            ], [
+                'category_id' => 5,
+                'type_id' => 6
+            ], [
+                'category_id' => 5,
+                'type_id' => 7
+            ], [
+                'category_id' => 5,
+                'type_id' => 8
+            ], [
+                'category_id' => 5,
+                'type_id' => 9
+            ], [
+                'category_id' => 5,
+                'type_id' => 10
+            ], [
+                'category_id' => 5,
+                'type_id' => 1
+            ], [
+                'category_id' => 5,
+                'type_id' => 2
+            ], [
+                'category_id' => 5,
+                'type_id' => 3
+            ], [
+                'category_id' => 5,
+                'type_id' => 4
+            ], [
+                'category_id' => 5,
+                'type_id' => 5
+            ]
+        ]);
+    }
+}
