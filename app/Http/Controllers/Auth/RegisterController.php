bbe36ef835d5775fb24ee3fc9cc0fbce824e15f1
MenschLennart <55825932+MenschLennart@users.noreply.github.com>
2020-04-09 21:31:13 +0200

    Login required now for contribution

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,8 +53,8 @@ class RegisterController extends Controller
             'username' => ['required', 'string', 'min:4', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'firstname' => ['string', 'max:100'],
-            'lastname' => ['string', 'max:100'],
+            'firstname' => ['nullable','string|max:100'],
+            'lastname' => ['nullable','string|max:100'],
         ]);
     }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,11 +37,15 @@
                              height="{{ env('TIC_CARDS_CAT_IMG_SIZE', 100) }}" alt="">
                         <h2>{{ $category->name }}</h2>
                         <p>{{ $category->description }}</p>
-                        <a href="#" class="btn btn-category-{{ $category->id }}" data-toggle="modal"
-                           data-target="#category_modal"
-                           data-title="{{ $category->name }}"
-                           data-category_id="{{ $category->id }}"
-                           data-types="{{ $category->types }}">{{ __('Contribute!') }}</a>
+                        @if(Auth()->guest())
+                            <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#loginOrRegisterModal">{{ __('Contribute!') }}</button>
+                        @else
+                            <a href="#" class="btn btn-category-{{ $category->id }}" data-toggle="modal"
+                               data-target="#category_modal"
+                               data-title="{{ $category->name }}"
+                               data-category_id="{{ $category->id }}"
+                               data-types="{{ $category->types }}">{{ __('Contribute!') }}</a>
+                        @endif
                         <a href="{{ route('categories.show', $category->id) }}"
                            class="btn btn-category-{{ $category->id }}">{{ __('Discover ') }} <span
                                 class="badge badge-pill badge-light">{{ ($category->activities->count()) }}</span></a>
@@ -99,68 +103,91 @@
             @endforeach
         @endisset
 
-        <div class="modal fade" id="category_modal" tabindex="-1" role="dialog"
-             aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="activityTitle"></h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
+            @if(Auth()->guest())
+                <div class="modal fade" id="loginOrRegisterModal" tabindex="-1" role="dialog" aria-labelledby="loginOrRegisterModal" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalCenterTitle">Login or Register</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                It's great you want to contribute. You're just little step away! Please login, or register an account.
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <a href="{{ route('login') }}" type="button" class="btn btn-primary">Login</a>
+                                <a href="{{ route('register') }}" type="button" class="btn btn-primary">Register</a>
+                            </div>
+                        </div>
                     </div>
-                    <form action="/activities" method="post">
-                        @csrf
-                        <div class="modal-body text-left">
+                </div>
+            @else
+                <div class="modal fade" id="category_modal" tabindex="-1" role="dialog"
+                     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="activityTitle"></h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <form action="/activities" method="post">
+                                @csrf
+                                <div class="modal-body text-left">
 
-                            <!-- Start form to add activity -->
-                            <div class="form-group row">
-                                <label for="inputTitle" class="col-sm-2 col-form-label">Title</label>
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="inputTitle" name="title"
-                                           placeholder="Nice App" required>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="inputContent" class="col-sm-2 col-form-label">Description</label>
-                                <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="inputContent" name="content"
-                                           placeholder="This app makes you happy..." required>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="inputCategory" class="col-sm-2 col-form-label">Category</label>
-                                <div class="col-sm-10">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="category_id"
-                                               id="inputCategory" checked required>
-                                        <label id="labelCategory" class="form-check-label"
-                                               for="inputCategory"></label>
+                                    <!-- Start form to add activity -->
+                                    <div class="form-group row">
+                                        <label for="inputTitle" class="col-sm-2 col-form-label">Title</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="inputTitle" name="title"
+                                                   placeholder="Nice App" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="inputContent" class="col-sm-2 col-form-label">Description</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="inputContent" name="content"
+                                                   placeholder="This app makes you happy..." required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="inputCategory" class="col-sm-2 col-form-label">Category</label>
+                                        <div class="col-sm-10">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="radio" name="category_id"
+                                                       id="inputCategory" checked required>
+                                                <label id="labelCategory" class="form-check-label"
+                                                       for="inputCategory"></label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="inputType" class="col-sm-2 col-form-label">Type</label>
+                                        <div id="typesArea" class="col-sm-10"></div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="inputLink" class="col-sm-2 col-form-label">Link</label>
+                                        <div class="col-sm-10">
+                                            <input type="url" class="form-control" id="inputLink" name="link"
+                                                   placeholder="https://" required>
+                                            <small id="urlHelp" class="form-text text-muted">Dont forget to put https://
+                                                or http:// at the beginning.</small>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="inputType" class="col-sm-2 col-form-label">Type</label>
-                                <div id="typesArea" class="col-sm-10"></div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="inputLink" class="col-sm-2 col-form-label">Link</label>
-                                <div class="col-sm-10">
-                                    <input type="url" class="form-control" id="inputLink" name="link"
-                                           placeholder="https://" required>
-                                    <small id="urlHelp" class="form-text text-muted">Dont forget to put https://
-                                        or http:// at the beginning.</small>
+                                <div class="modal-footer">
+                                    <button id="abortButton" type="button" class="btn btn-secondary" data-dismiss="modal">Abort</button>
+                                    <button id="submitButton" type="submit" class="btn">Add</button>
                                 </div>
-                            </div>
+                            </form>
                         </div>
-                        <div class="modal-footer">
-                            <button id="abortButton" type="button" class="btn btn-secondary" data-dismiss="modal">Abort</button>
-                            <button id="submitButton" type="submit" class="btn">Add</button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
-        </div>
+            @endif
 
     </div>
 @endsection
